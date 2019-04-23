e1cd8c3d550c987d76031f43c5187591d957e028
David Cabrera <david@rerainc.com>
2019-04-22 22:16:56 -0400

    add status endpoint

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/status', function () {
+    return response("Success", 200)
+      ->header('Content-Type', 'text/plain');
+});
